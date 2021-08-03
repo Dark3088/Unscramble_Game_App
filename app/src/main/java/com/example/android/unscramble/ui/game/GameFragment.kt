824a6792aea5254adf1c886bce099782a95e3f58
Diego Rocha <diegorsf.dns@gmail.com>
2021-08-03 04:29:01 -0300

    Implemented an observer object
    - Got rid of unnecessary code

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -27,9 +27,6 @@ import com.example.android.unscramble.R
 import com.example.android.unscramble.databinding.GameFragmentBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-/**
- * Fragment where the game is played, contains the game logic.
- */
 class GameFragment : Fragment() {
 
     // Binding object instance with access to the views in the game_fragment.xml layout
@@ -60,10 +57,16 @@ class GameFragment : Fragment() {
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
         // Update the UI
-        updateNextWordOnScreen()
+
         binding.score.text = getString(R.string.score, 0)
         binding.wordCount.text = getString(
                 R.string.word_count, 0, MAX_NO_OF_WORDS)
+
+        // Observe the scrambledCharArray LiveData, passing in the LifecycleOwner and the observer.
+        viewModel.currentScrambledWord.observe(viewLifecycleOwner,
+            { newWord ->
+                binding.textViewUnscrambledWord.text = newWord
+            })
     }
 
     /*
@@ -77,9 +80,9 @@ class GameFragment : Fragment() {
         if (viewModel.isUserWordCorrect(playerWord)) {
             setErrorTextField(false)
 
-            if (viewModel.nextWord()) { updateNextWordOnScreen() }
-            else                      { showFinalScoreDialog()   }
-
+            if (!viewModel.nextWord()) {
+                showFinalScoreDialog()
+            }
         } else {  setErrorTextField(true) }
     }
 
@@ -91,22 +94,14 @@ class GameFragment : Fragment() {
         if (viewModel.nextWord())
         {
             setErrorTextField(false)
-            updateNextWordOnScreen()
+
 
          } else { showFinalScoreDialog() }
     }
 
-    /*
-     * Gets a random word for the list of words and shuffles the letters in it.
-     */
-    private fun getNextScrambledWord(): String {
-        val tempWord = allWordsList.random().toCharArray()
-        tempWord.shuffle()
-        return String(tempWord)
-    }
-
     // Creates an Alert Dialog for displaying the user score
-    private fun showFinalScoreDialog(){
+    private fun showFinalScoreDialog()
+    {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.congratulations))
             .setMessage(getString(R.string.you_scored, viewModel.score))
@@ -120,37 +115,30 @@ class GameFragment : Fragment() {
      * Re-initializes the data in the ViewModel and updates the views with the new data, to
      * restart the game.
      */
-    private fun restartGame() {
+    private fun restartGame()
+    {
         viewModel.reinitializeData()
         setErrorTextField(false)
-        updateNextWordOnScreen()
+
     }
 
     /*
      * Exits the game.
      */
-    private fun exitGame() {
-        activity?.finish()
-    }
+    private fun exitGame() { activity?.finish() }
 
     /*
     * Sets and resets the text field error status.
     */
-    private fun setErrorTextField(error: Boolean) {
-        if (error) {
+    private fun setErrorTextField(error: Boolean)
+    {
+        if (error)
+        {
             binding.textField.isErrorEnabled = true
             binding.textField.error = getString(R.string.try_again)
         } else {
             binding.textField.isErrorEnabled = false
             binding.textInputEditText.text = null
         }
-    }
-
-    /*
-     * Displays the next scrambled word on screen.
-     */
-    private fun updateNextWordOnScreen() {
-        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
-
     }
 }
